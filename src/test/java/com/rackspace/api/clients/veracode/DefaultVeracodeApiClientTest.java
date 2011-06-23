e331e94b3a4bf0dfd112339951daaf3b56a68adf
John Madrid <john.madrid@M147AGY.rackspace.corp>
2011-06-23 15:45:42 -0500

    added a test method for the veracode api

---
+++ src/test/java/com/rackspace/api/clients/veracode/DefaultVeracodeApiClientTest.java
@@ -0,0 +1,27 @@
+package com.rackspace.api.clients.veracode;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: john.madrid
+ * Date: 6/23/11
+ * Time: 3:25 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class DefaultVeracodeApiClientTest {
+    DefaultVeracodeApiClient client;
+
+    @Before
+    public void setUp() throws Exception {
+        client = new DefaultVeracodeApiClient("https://analysiscenter.veracode.com/api/", "monitoring-api", "YOUR_PASSWORD_HERE", System.out);
+    }
+
+    @Test
+    public void testGetAppId() throws Exception {
+        String appId = client.getAppId("RADAR");
+
+        System.out.println(appId);
+    }
+}
